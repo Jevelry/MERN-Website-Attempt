eaa8ab5f80df10b9761bd704e7b99bffd0c114ba
Kevin <kevin.huang6541@gmail.com>
2022-05-22 22:07:29 +1000

    finished website. code cleanup needed

--- restaurants/backend/index.js
+++ restaurants/backend/index.js
@@ -1,6 +1,104 @@
 const express = require("express")
+const {google} = require("googleapis")
+const cors = require("cors")
+const bp = require('body-parser')
+const { type } = require("express/lib/response")
 const app = express()
-
+app.use(cors())
+app.use(bp.json())
 app.listen(3001, ()=> {
-    console.log("hello")
+})
+app.get("/", (req,res) => {
+})
+
+app.post("/", async (req,res) => {
+})
+
+app.post("/submit", async (req,res) => {
+    // REMINDER GET FRONTEND TO CHECK FORMAT
+    const {name, location, date, comments, score, author} = req.body
+    // get authentication data from secrets file
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "secrets.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+    // Create client
+    const client = await auth.getClient()
+    // Create instance of google sheets
+    const googleSheets = google.sheets({
+        version: "v4", 
+        auth: client
+    })
+    values = capitalizeWords([name, location, date, comments, score, author])
+    try {
+        const res = await googleSheets.spreadsheets.values.append({
+            spreadsheetId: "17NpdIhg8e5MtouDL28aCkEViSZl-UQSOnjFShQoX-zE",
+            range: "Sheet1!A:F",
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: [values]
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+// Gets name and location of all restaurants
+app.get("/getRestaurants", async (req,res) => {
+    // get authentication data from secrets file
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "secrets.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+    // Create client
+    const client = await auth.getClient()
+    // Create instance of google sheets
+    const googleSheets = google.sheets({
+        version: "v4", 
+        auth: client
+    })
+    const getRows = await googleSheets.spreadsheets.values.get({
+        spreadsheetId: "17NpdIhg8e5MtouDL28aCkEViSZl-UQSOnjFShQoX-zE",
+        range: "Sheet1!A:B"
+    })
+    let data = getRows.data.values
+    let uniqueArray = Array.from(new Set(data.map(JSON.stringify)), JSON.parse);
+    res.send(uniqueArray)
+})
+function capitalizeWords(arr) {
+  return arr.map(element => {
+    return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase();
+  });
+}
+// Gets form data for a particular restaurant
+app.get("/name/:nameId/location/:locationId", async (req,res) => {
+    // Get url parameters
+    const name = req.params.nameId
+    const location = req.params.locationId
+    // get authentication data from secrets file
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "secrets.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+    // Create client
+    const client = await auth.getClient()
+    // Create instance of google sheets
+    const googleSheets = google.sheets({
+        version: "v4", 
+        auth: client
+    })
+    const getRows = await googleSheets.spreadsheets.values.get({
+        spreadsheetId: "17NpdIhg8e5MtouDL28aCkEViSZl-UQSOnjFShQoX-zE",
+        range: "Sheet1"
+    })
+    const data = getRows.data.values
+    
+    let output = []
+    for (const entry of data) {
+        if (entry[0] === name && entry[1] === location) {
+            output.push(entry)
+        }
+    }
+    res.send(output)
 })
--- restaurants/frontend/src/App.js
+++ restaurants/frontend/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/home/home"
+import Form from "./components/form/form"
+import Restaurants from "./components/table/restaurants"
+import Info from "./components/info/info"
+
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 function App() {
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    }); 
+    const [open, setOpen] = React.useState(false);
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+        return;
+        }
+        setOpen(false);
+    };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='App'>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form setOpen = {setOpen}/>} />
+          <Route path="/restaurants" element={<Restaurants />} />
+          <Route path="/getRestaurant/:name/:location" element={<Info />} />
+        </Routes>
+      </Suspense>
+    </Router>
+
+    <Snackbar open={open} anchorOrigin={{vertical: 'top', horizontal: 'center'}} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+            Success
+        </Alert>
+    </Snackbar>
+  </div>
+
   );
 }
-
 export default App;
